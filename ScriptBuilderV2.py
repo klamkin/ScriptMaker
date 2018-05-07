eb7cb59a4b3ba646c6f6554564a09d462a8288b1
klamkin <31418136+klamkin@users.noreply.github.com>
2018-05-07 18:58:27 -0300

    Add files via upload

---
+++ ScriptBuilderV2.py
@@ -0,0 +1,32 @@
+arr = []
+
+def only_numerics(seq):
+    seq_type= type(seq)
+    return seq_type().join(filter(seq_type.isdigit, seq))
+
+code = ''
+
+print ('Paste output here: ')
+while (code != 'q'):
+	code = input()
+	if code != 'q':
+		code = only_numerics(code)
+		arr.append(code)
+	
+	
+
+f= open("RenameMeToSomethingUsefulAndNotAsLong.script","w+")	
+
+f.write("1" + "\n")
+f.write(str(len(arr)) + "\n")
+for i in range(len(arr)):
+	f.write("1" + "\n")
+f.write("\n")
+
+for i in range(len(arr)):
+	f.write(str(arr[i]) + "\n")
+
+print(arr)
+
+
+code = input()
