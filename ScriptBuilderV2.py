da16347740aaad524fa520a6b90d8ee95b2b1982
klamkin <31418136+klamkin@users.noreply.github.com>
2018-05-08 14:30:26 -0300

    Add files via upload

--- ScriptBuilderV2.py
+++ ScriptBuilderV2.py
@@ -1,29 +1,53 @@
+import os.path
+isThere = True
+
+# Declaring the array used to store the values
 arr = []
 
+# Method to filter out any non numeric chars
 def only_numerics(seq):
     seq_type= type(seq)
     return seq_type().join(filter(seq_type.isdigit, seq))
 
 code = ''
+
+# Print out of instructions 
 print ('Script Builder - Kyle Lamkin')
 print ('\n')
 print ('Instructions \n')
-print ('1. Copy output (ctrl + c) from desired game in EGM commander \n')
-print ('2. Paste into this program (ctrl + v) \n')
-print ('3. Hit Enter \n')
-print ('4. Type "q" and hit Enter \n')
+print ('1. Enter a name for your script (Be sure to be descriptive)')
+print ('2. Copy output (ctrl + c) from desired game in EGM commander \n')
+print ('3. Paste into this program (ctrl + v) \n')
+print ('4. Hit Enter \n')
+print ('5. Type "q" and hit Enter \n')
 print ('NOTE - Remember to rename your script to something descriptive \n')
+name = input('Please enter a name for your script: \n')
+while (isThere):
+	if os.path.isfile(name + ".script"):
+		isThere = True
+		print ("That name already exists, Please choose a different name")
+	else:
+		isThere = False
+		
+	
+
 print ('Paste output here: ' + '\n')
+
+# The while loop that accepts input for the script
 while (code != 'q'):
 	code = input()
+	
+	# If the input is 'q' then the program ends and compiles the script
 	if code != 'q':
 		code = only_numerics(code)
 		arr.append(code)
+	
+	# This pops off any blank lines in the array. This can happen when a line with no numeric chars is entered.
 	if not code:
 		arr.pop()
-	
+#Attempts to write to a file	
 try:
-	f= open("RenameMeToSomethingUsefulAndNotAsLong.script","w+")	
+	f= open(name + ".script","w+")	
 
 	f.write("rng vals" + "\n")
 	f.write("\n")
